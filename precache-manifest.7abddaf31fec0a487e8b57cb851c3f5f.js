1ec877b446029555c836bd06d64f4e4100fc59d5
Soham Jog <jogsoham2003@gmail.com>
2022-06-03 14:56:24 +0530

    Updates

---
+++ precache-manifest.7abddaf31fec0a487e8b57cb851c3f5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178c2da880852420cda37c781036333e",
+    "url": "/votingDapp/index.html"
+  },
+  {
+    "revision": "6e827d95d95ba4fd756c",
+    "url": "/votingDapp/static/css/main.0aa04b40.chunk.css"
+  },
+  {
+    "revision": "3605bb1967b6afeea559",
+    "url": "/votingDapp/static/js/2.59e4bfc8.chunk.js"
+  },
+  {
+    "revision": "6e827d95d95ba4fd756c",
+    "url": "/votingDapp/static/js/main.db5e6bac.chunk.js"
+  },
+  {
+    "revision": "6c3a964537db93d1a49d",
+    "url": "/votingDapp/static/js/runtime-main.e8fb00c6.js"
+  }
+]);
